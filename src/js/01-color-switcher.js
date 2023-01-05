78a09707ad19cf25ae9bff18f0820aba89748ada
Aaron MV <epsilon11101@gmail.com>
2023-01-05 12:39:50 -0600

    finished first exercise

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,2 +1,19 @@
 import common from "../css/common.css";
 import data from "../data.json";
+
+const start = document.querySelector("[data-start]");
+const stop = document.querySelector("[data-stop]");
+let second;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+start.addEventListener("click", () => {
+  second = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+stop.addEventListener("click", () => {
+  clearTimeout(second);
+});
